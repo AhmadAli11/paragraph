01553f2241277d6c82e25748316b5f0cc41d6b03
Ahmad Ali <electroohamood@gmail.com>
2021-05-14 04:35:10 -0700

    creat main project structure

---
+++ js/bgc2.js
@@ -0,0 +1,15 @@
+﻿let
+	buttonW = document.getElementById('cl1'),
+	buttonB = document.getElementById('cl2');
+
+buttonW.onclick = () => {
+    document.body.style.backgroundColor = '#fff';
+	document.getElementById('slash').style.color = '#131313';
+	document.getElementById('f').style.backgroundColor = '#fff';
+};
+
+	buttonB.onclick = () => {
+	document.body.style.backgroundColor = '#131313';
+	document.getElementById('slash').style.color = '#fff';
+	document.getElementById('f').style.backgroundColor = '#131313';
+	};
---
+++ js/bgcm.js
@@ -0,0 +1,11 @@
+﻿let
+	buttonW = document.getElementById('cl1'),
+	buttonB = document.getElementById('cl2');
+
+buttonW.onclick = () => {
+	document.body.style.backgroundColor = '#fff';
+};
+
+buttonB.onclick = () =>{
+	document.body.style.backgroundColor = '#131313';
+};
